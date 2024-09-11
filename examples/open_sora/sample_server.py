7d6202b7e3d48c44403912902b0fe1e12f23c91b
duihuhu <duihuhu@163.com>
2024-09-11 16:28:37 +0800

    debug add generate server

--- examples/open_sora/sample_server.py
+++ examples/open_sora/sample_server.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     # run_pab()
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, defauelt="127.0.0.1")
+    parser.add_argument("--host", type=str, default="127.0.0.1")
     parser.add_argument("--port", type=int, default=8000)
 
     args = parser.parse_args()
