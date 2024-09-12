504d0bca0496ab40c06ea16767cb4b0c4aa46616
duihuhu <duihuhu@163.com>
2024-09-12 10:53:41 +0800

    debug add generate server

--- videosys/pipelines/open_sora/pipeline_open_sora.py
+++ videosys/pipelines/open_sora/pipeline_open_sora.py
@@ -614,7 +614,7 @@ class OpenSoraPipeline(VideoSysPipeline):
                 mask=masks,
             )
             samples = self.vae.decode(samples.to(self._dtype), num_frames=num_frames)
-            print("type samples ", type(samples))
+            print("type samples ", type(samples), samples.shape)
             video_clips.append(samples)
 
         for i in range(1, loop):
