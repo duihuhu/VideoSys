528593908645d6238989951f437f68cb49fd5665
duihuhu <duihuhu@163.com>
2024-09-13 11:01:57 +0800

    debug add generate server

--- examples/open_sora/async_engine.py
+++ examples/open_sora/async_engine.py
@@ -181,6 +181,7 @@ class AsyncEngine:
 
     async def run_engine_loop(self):
         has_requests_in_progress = False
+        print("run_engine_loop ")
         while True:
             if (not has_requests_in_progress and
                 not self.engine.scheduler.recv_transfering and
