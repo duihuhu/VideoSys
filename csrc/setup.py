b764c3df9b13ddd11219ccc2513c08ab88f17874
duihuhu <duihuhu@163.com>
2024-09-14 17:02:43 +0800

    debug add generate server

--- csrc/setup.py
+++ csrc/setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext
-import sys
 import os
 import numpy
 
-# 自定义 CUDAExtension 类来处理 CUDA 编译
+# 自定义 CUDAExtension 类
 class CUDAExtension(Extension):
     def __init__(self, name, sources, include_dirs=None, library_dirs=None, libraries=None, extra_compile_args=None):
         super().__init__(name, sources)
@@ -18,9 +17,7 @@ class CUDAExtension(Extension):
 class BuildExt(build_ext):
     def build_extensions(self):
         # 设置编译器
-        compiler = self.compiler.compiler_so[0]
         for ext in self.extensions:
-            # 如果是 CUDA 文件，使用 nvcc 进行编译
             if any(source.endswith(".cu") for source in ext.sources):
                 self._compile_cuda(ext)
             else:
@@ -43,7 +40,7 @@ class BuildExt(build_ext):
 
                 # 处理 include 路径和其他编译选项
                 nvcc_cmd += ['-I' + inc for inc in ext.include_dirs]
-                nvcc_cmd += ext.extra_compile_args or []
+                nvcc_cmd += ext.extra_compile_args.get('nvcc', [])  # 只传递 nvcc 编译器的选项
 
                 # 执行编译命令
                 print(f"Running nvcc command: {' '.join(nvcc_cmd)}")
@@ -56,16 +53,19 @@ class BuildExt(build_ext):
 # 包含 CUDA 和 C++ 源文件的扩展模块
 ext_modules = [
     CUDAExtension(
-        name="csrc",
+        name="trans_ops",
         sources=["pybind.cpp", "trans_manager.cu"],  # 包含 C++ 和 CUDA 文件
         include_dirs=[numpy.get_include()],          # 例如添加 numpy 的头文件
-        extra_compile_args={'cxx': ['-O3'], 'nvcc': ['-O3']}  # 添加编译选项
+        extra_compile_args={
+            'cxx': ['-O3'],        # C++ 编译器选项
+            'nvcc': ['-O3']        # CUDA 编译器选项
+        }
     )
 ]
 
 # setup 函数
 setup(
-    name="csrc",
+    name="trans_ops",
     version="0.1",
     ext_modules=ext_modules,
     cmdclass={'build_ext': BuildExt},
