4db510b3334ecf0a48cf4c3b17fff1455caff679
duihuhu <duihuhu@163.com>
2024-09-18 10:34:32 +0800

    debug add generate server

--- csrc/pybind.cpp
+++ csrc/pybind.cpp
@@ -2,11 +2,11 @@
 #include <torch/extension.h>
 
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+PYBIND11_MODULE(video_ops, m) {
   pybind11::module trans_ops = m.def_submodule("trans_ops", "video gpu nccl utils");
 
-//   py::class_<TransManager>(trans_ops, "TransManager")
-//       .def(py::init<int, std::vector<std::pair<at::Tensor, at::Tensor>>&, int, int , int, int, int, int, std::vector<uint64_t>& >())
-//       .def("get_nccl_id", &TransManager::get_nccl_id, "A function that returns NCCL unique ID as a list of characters")
-//       .def("create_comm", &TransManager::create_comm, "A function create comm")
+  py::class_<TransManager>(trans_ops, "TransManager")
+      .def(py::init<int, std::vector<std::pair<at::Tensor, at::Tensor>>&, int, int , int, int, int, int, std::vector<uint64_t>& >())
+      .def("get_nccl_id", &TransManager::get_nccl_id, "A function that returns NCCL unique ID as a list of characters")
+      .def("create_comm", &TransManager::create_comm, "A function create comm")
 }
