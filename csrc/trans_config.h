778c1fd4030eaf92a202ec0cd32889f0761e6e5e
duihuhu <duihuhu@163.com>
2024-09-18 09:48:00 +0800

    debug add generate server

--- csrc/trans_config.h
+++ csrc/trans_config.h
@@ -11,7 +11,7 @@
 #include <utility>
 #include <c10/cuda/CUDAStream.h>
 #include <ATen/cuda/CUDAEvent.h>
-#include "nccl.h"
+#include <nccl.h>
 #include <nlohmann/json.hpp>  // Include the JSON library
 #include <iostream>
 #include <cuda_runtime.h>
