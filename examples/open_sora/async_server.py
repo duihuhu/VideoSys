cb9ccee502893cb87c904ee54a97c805f03139d5
duihuhu <duihuhu@163.com>
2024-09-19 09:08:10 +0800

    debug worker rank generate server

--- examples/open_sora/async_server.py
+++ examples/open_sora/async_server.py
@@ -124,7 +124,6 @@ if __name__ == "__main__":
     parser.add_argument("--host", type=str, default="127.0.0.1")
     parser.add_argument("--port", type=int, default=8000)
     parser.add_argument("--num_gpus", type=int, default=1)
-    parser.add_argument("--num_gpus", type=int, default=1)
     parser.add_argument('--enable-separate', action="store_true", help=('separate or not '))
     parser.add_argument('--role', type=str, choices=['dit', 'vae'], default=None, help=('instance '))
     args = parser.parse_args()
