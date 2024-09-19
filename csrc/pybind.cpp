e8ce87241c987d6eda5236252a18ff2c5ce2b985
duihuhu <duihuhu@163.com>
2024-09-19 14:42:09 +0800

    debug worker rank generate server

--- csrc/pybind.cpp
+++ csrc/pybind.cpp
@@ -6,7 +6,7 @@ PYBIND11_MODULE(video_ops, m) {
   pybind11::module trans_ops = m.def_submodule("trans_ops", "video gpu nccl utils");
 
   py::class_<TransManager>(trans_ops, "TransManager")
-      .def(py::init<int , int, int>())
+      .def(py::init<int , int, string>())
       .def("get_nccl_id", &TransManager::get_nccl_id, "A function that returns NCCL unique ID as a list of characters")
       .def("create_comm", &TransManager::create_comm, "A function create comm");
 }
