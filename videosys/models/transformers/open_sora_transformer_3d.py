0b1a9814c379dd19c7b219dd1f84f9fed523ff35
duihuhu <duihuhu@163.com>
2024-11-24 22:21:41 +0800

    debug transformer and vae

--- videosys/models/transformers/open_sora_transformer_3d.py
+++ videosys/models/transformers/open_sora_transformer_3d.py
@@ -204,30 +204,30 @@ class STDiT3Block(nn.Module):
 
             # attention
             if self.temporal:
-                import time
-                t1 = time.time()
+                # import time
+                # t1 = time.time()
                 if enable_sequence_parallel():
                     x_m, S, T = self.dynamic_switch(x_m, S, T, to_spatial_shard=True)
                 x_m = rearrange(x_m, "B (T S) C -> (B S) T C", T=T, S=S)
-                torch.cuda.synchronize()
-                t2 = time.time()
+                # torch.cuda.synchronize()
+                # t2 = time.time()
                 x_m = self.attn(x_m)
                 x_m = rearrange(x_m, "(B S) T C -> B (T S) C", T=T, S=S)
                 if enable_sequence_parallel():
-                    torch.cuda.synchronize()
-                    t3 = time.time()
+                    # torch.cuda.synchronize()
+                    # t3 = time.time()
                     x_m, S, T = self.dynamic_switch(x_m, S, T, to_spatial_shard=False)
-                    torch.cuda.synchronize()
-                    t4 = time.time()
-                    print("STDiTBlock ", t4-t3, t3-t2, t2-t1)
+                    # torch.cuda.synchronize()
+                    # t4 = time.time()
+                    # print("STDiTBlock ", t4-t3, t3-t2, t2-t1)
             else:
                 x_m = rearrange(x_m, "B (T S) C -> (B T) S C", T=T, S=S)
-                import time
-                t1 = time.time()
+                # import time
+                # t1 = time.time()
                 x_m = self.attn(x_m)
-                torch.cuda.synchronize()
-                t2 = time.time()
-                print("STDiTBlock ", t2-t1)
+                # torch.cuda.synchronize()
+                # t2 = time.time()
+                # print("STDiTBlock ", t2-t1)
                 x_m = rearrange(x_m, "(B T) S C -> B (T S) C", T=T, S=S)
 
             # modulate (attention)
