e6f45d88a854282066730061e8a4beb90947f716
duihuhu <duihuhu@163.com>
2024-11-27 15:37:58 +0800

    debug add init global sched

--- videosys/core/engine.py
+++ videosys/core/engine.py
@@ -192,8 +192,6 @@ class VideoSysEngine:
         for (worker_id, idx) in zip(worker_ids, range(len(worker_ids))):
             if idx == 0:
                 args = (*args, True)
-            else:
-                args = (*args, True)
             worker_outputs.append(self.workers[worker_id].execute_method_async(method, *args, **kwargs))
         
         if async_run_tensor_parallel_workers_only:
