9fa5688572c1cb1f8c754972896b365ee694e181
unknown <3328677832@qq.com>
2024-12-14 09:40:04 +0800

    add prcoess log

---
+++ examples/global_scheduler/process.py
@@ -0,0 +1,20 @@
+with open("/home/jovyan/hhy/VideoSys/examples/global_scheduler/logs.txt", 'r') as file:
+    lines = file.readlines()
+    start_time = float(lines[0].strip().split(' ')[-1])
+    datas = []
+    for i, line in enumerate(lines):
+        if i == 0:
+            continue
+        else:
+            data = line.strip().split(' ')
+            id = int(line[0])
+            end_time = float(line[4])
+            resolution = str(line[-1])  
+            datas.append((id,end_time-start_time,resolution))
+    datas.sort(key=lambda x:x[0])
+    for data in datas:
+        print(data[0])
+    for data in datas:
+        print(data[1])
+    for data in datas:
+        print(data[2])
