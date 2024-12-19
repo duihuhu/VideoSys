468428a142c24353057f1b7ec0fe0c1b674d9dab
duihuhu <duihuhu@163.com>
2024-12-19 19:09:05 +0800

    debug init videosys worker parallism

--- videosys/core/scheduler.py
+++ videosys/core/scheduler.py
@@ -13,7 +13,7 @@ class VideoScheduler:
     def schedule(self) -> SequenceGroup:
         if self.waiting:
             seq_group = self.waiting[0]
-            worker_ids = [0]
+            worker_ids = [0,1]
             self.gpu_status = [1,1]
             seq_group.worker_ids = worker_ids
             self.waiting.popleft()
