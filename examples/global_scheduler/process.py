480f640f931a2c12a1d448bb5678cf77b1eac60d
FredHuang99 <3328677832@qq.com>
2025-01-09 23:37:51 +0800

    non-lock debug11

--- examples/global_scheduler/process.py
+++ examples/global_scheduler/process.py
@@ -3,6 +3,15 @@ prefix_path = "/home/jovyan/hhy/VideoSys/examples/global_scheduler/logs_"
 suffix_path = ".txt"
 ratios = [(1,1,8),(2,2,6),(3,3,4),(1,8,1),(2,6,2),(3,4,3),(8,1,1),(6,2,2),(4,3,3),(2,4,4),(4,2,4),(4,4,2),(1,3,6),(6,1,3),(3,6,1),(1,1,1)]
 
+for x, y, z in ratios:
+    print(f"----------Ratio----------")
+    print(f"144p: 240p: 360p = {x}: {y}: {z}")
+    file_path = prefix_path + str(x) + "_" + str(y) + "_" + str(z) + suffix_path
+    with open(file_path, 'w') as file:
+        lines = file.readlines()
+        for line in lines:
+            print(line)
+
 #count = 0
 for x, y, z in ratios:
     #for j, policy in enumerate(schedule_policies):
