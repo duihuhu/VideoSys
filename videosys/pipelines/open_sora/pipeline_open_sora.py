93f56aa13773d3fb43dacdab9fceaff6dcf5bdd9
duihuhu <duihuhu@163.com>
2025-01-14 19:18:24 +0800

    debug generate toghter

--- videosys/pipelines/open_sora/pipeline_open_sora.py
+++ videosys/pipelines/open_sora/pipeline_open_sora.py
@@ -660,6 +660,11 @@ class OpenSoraPipeline(VideoSysPipeline):
             samples = self.vae.decode(samples.to(self._dtype), num_frames=num_frames)
             torch.cuda.synchronize() 
             t3 = time.time()
+            dt3 = datetime.fromtimestamp(int(t1))  # 转换为秒的 datetime
+            ms3 = int((t3 - int(t3)) * 1000)  # 提取毫秒部分
+            # 格式化输出
+            formatted_time3 = dt3.strftime("%Y-%m-%d %H:%M:%S") + f".{ms3:03d}"
+            print("vae start time ", formatted_time3)
             print("transformer, vae ", t2-t1, t3-t2)
             video_clips.append(samples)
 
