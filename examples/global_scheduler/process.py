110fcf0bf6a010249f63047e1dee5a074554f5bd
FredHuang99 <3328677832@qq.com>
2025-01-15 01:42:48 +0800

    couple test2 + 1

--- examples/global_scheduler/process.py
+++ examples/global_scheduler/process.py
@@ -1,7 +1,7 @@
 prefix_path = "/home/jovyan/hcch/hucc/VideoSys/examples/global_scheduler/batch_high_affinity2/"
 prefix_path2 = "/home/jovyan/hcch/hucc/VideoSys/examples/global_scheduler/logs_temp/"
 prefix_path3 = "/home/jovyan/hhy/logs/"
-ratios = [(1,1,8),(2,2,6),(3,3,4),(1,8,1),(2,6,2),(3,4,3),(8,1,1),(6,2,2),(4,3,3),(2,4,4),(4,2,4),(4,4,2),(1,3,6),(6,1,3),(3,6,1),(1,1,1)]
+ratios = [(2,2,6),(2,6,2),(6,2,2),(2,4,4),(4,2,4),(4,4,2),(1,3,6),(6,1,3),(3,6,1),(1,1,1)]
 
 
 ddits = []
