802ec02f2292ef1c64d84b6ad1d5a7b6c1224f4b
FredHuang99 <3328677832@qq.com>
2025-01-14 17:45:33 +0800

    process3

--- examples/global_scheduler/process.py
+++ examples/global_scheduler/process.py
@@ -1,5 +1,5 @@
 prefix_path = "/home/jovyan/hcch/hucc/VideoSys/examples/global_scheduler/batch_high_affinity2/"
-prefix_path2 = "/home/jovyan/hcch/hucc/VideoSys/examples/global_scheduler/logs_temp"
+prefix_path2 = "/home/jovyan/hcch/hucc/VideoSys/examples/global_scheduler/logs_temp/"
 ratios = [(1,1,8),(2,2,6),(3,3,4),(1,8,1),(2,6,2),(3,4,3),(8,1,1),(6,2,2),(4,3,3),(2,4,4),(4,2,4),(4,4,2),(1,3,6),(6,1,3),(3,6,1),(1,1,1)]
 
 
