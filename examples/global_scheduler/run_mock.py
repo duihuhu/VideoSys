4f6a5a1a2927d1b9a47d82cefaba4921c2c0ec47
FredHuang99 <3328677832@qq.com>
2025-01-14 14:55:17 +0800

    batch high affinity test starts

--- examples/global_scheduler/run_mock.py
+++ examples/global_scheduler/run_mock.py
@@ -2,7 +2,7 @@ import os
 import time
 
 ratios = [(1,1,8),(2,2,6),(3,3,4),(1,8,1),(2,6,2),(3,4,3),(8,1,1),(6,2,2),(4,3,3),(2,4,4),(4,2,4),(4,4,2),(1,3,6),(6,1,3),(3,6,1),(1,1,1)]
-base_command = "python3 mock_test.py --low {x} --middle {y} --high {z} --log-file-path ..._{x}_{y}_{z}"
+base_command = "python3 mock_test.py --low {x} --middle {y} --high {z} --log-file-path /home/jovyan/hcch/hucc/VideoSys/examples/global_scheduler/batch_high_affinity/{x}_{y}_{z}_"
 
 for x, y, z in ratios:
     command = base_command.format(x = x, y = y, z = z)
