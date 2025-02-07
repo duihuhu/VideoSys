8410427b5458329348ca4faa95e4e8c57ed642a7
FredHuang99 <3328677832@qq.com>
2025-02-07 16:07:46 +0800

    opt gpu num 1 2 2 & hungry first

--- videosys/core/scheduler.py
+++ videosys/core/scheduler.py
@@ -31,8 +31,7 @@ class VideoScheduler:
                                                        "240p": {1: 6.83, 2: 3.20, 4: 2.17}, 
                                                        "360p": {1: 14.49, 2: 6.65, 4: 3.73, 8: 2.38}}
         
-        self.opt_gpus_num: Dict[str, int] = {"144p": 1, "240p": 2, "360p": 4}
-        #self.opt_gpus_num: Dict[str, int] = {"144p": 1, "240p": 2, "360p": 2}
+        self.opt_gpus_num: Dict[str, int] = {"144p": 1, "240p": 2, "360p": 2}
 
         self.denoising_steps = 30
 
