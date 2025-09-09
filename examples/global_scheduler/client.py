4d25728b660f67f68e38f89dee864e068e74ac44
unknown <3328677832@qq.com>
2025-09-10 00:36:23 +0800

    test bs

--- examples/global_scheduler/client.py
+++ examples/global_scheduler/client.py
@@ -74,8 +74,8 @@ def main(prompt, aspect_ratio, num_frames, res_path: str, recv_ratio: float, bat
                 if i < len(sleep_times) - 1:
                     time.sleep(sleep_times[i])
     else:
-        prompts = prompt * pbs
-        for _ in range(8):
+        prompts = [prompt] * pbs
+        for _ in range(1):
             post_request_and_get_response(prompts, tres, aspect_ratio, num_frames)
         '''add_resolutions = ['360p'] * 15
         for resolution in add_resolutions:
