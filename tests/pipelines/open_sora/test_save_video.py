1bd7b9cc09a4985572625ede95d208e574ffcbed
unknown <3328677832@qq.com>
2025-09-10 00:07:19 +0800

    test save starts

--- tests/pipelines/open_sora/test_save_video.py
+++ tests/pipelines/open_sora/test_save_video.py
@@ -9,7 +9,7 @@ shape = (1, 51, 144, 256, 3)
 # 然后转换为uint8并移动到CPU
 video = torch.randint(0, 256, shape, dtype=torch.uint8, device='cpu')
 
-output_path = os.path.join("/workspace/Videosys/outputs", "save_test.mp4")
+output_path = os.path.join("/workspace/VideoSys/outputs", "save_test.mp4")
     
 os.makedirs(os.path.dirname(output_path), exist_ok=True)
 imageio.mimwrite(output_path, video[0], fps=24)
