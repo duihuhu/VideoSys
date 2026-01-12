80c88b2f46e88fd4a5743d04fbf36fb45c483758
FredHuang99 <3328677832@qq.com>
2026-01-13 00:06:49 +0800

    rebuttal16

--- examples/global_scheduler/gs.py
+++ examples/global_scheduler/gs.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     parser.add_argument("--port", type=int, default=8001)
     parser.add_argument("--instances-num", type = int, default = 8)
     parser.add_argument("--static-dop", type = int, default = 4)
-    parser.add_argument("--window-size", type = int, default = 8)
+    parser.add_argument("--window-size", type = int, default = 16)
 
     args = parser.parse_args()
     sched = AsyncSched(instances_num = args.instances_num,
