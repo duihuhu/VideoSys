c524fdc4128b69e6c9521e505c9f7241d579279e
FredHuang99 <3328677832@qq.com>
2026-01-13 00:13:13 +0800

    rebuttal17

--- videosys/core/async_engine.py
+++ videosys/core/async_engine.py
@@ -328,8 +328,8 @@ class AsyncSched:
     
     def create_consumer(self, instances_num: int):
         for _ in range(instances_num):
-            #consumer = threading.Thread(target=self.process)
-            consumer = threading.Thread(target=self.process2)
+            consumer = threading.Thread(target=self.process)
+            #consumer = threading.Thread(target=self.process2)
             consumer.daemon = True
             consumer.start()
             self.consumers.append(consumer)
@@ -371,7 +371,7 @@ class AsyncSched:
         
     async def step_async(self):
         #t1 = time.time()
-        #seq_group = self.video_sched.scheduler.hungry_first_priority_schedule()
+        seq_group = self.video_sched.scheduler.hungry_first_priority_schedule()
         #t2 = time.time()
         #with open("costs.txt", "a") as file:    
         #    file.write(f"{t2-t1}\n") 
@@ -383,7 +383,7 @@ class AsyncSched:
         #seq_group = self.video_sched.scheduler.smart_dynamic_partition_schedule()
         #seq_group = self.video_sched.scheduler.sjf_priority_schedule()
         #seq_group = self.video_sched.scheduler.continuous_batching_schedule()
-        seq_group = self.video_sched.scheduler.window_based_sjf_schedule()
+        #seq_group = self.video_sched.scheduler.window_based_sjf_schedule()
         #seq_group = self.video_sched.scheduler.window_based_sjf_with_hungry_update_schedule()
         #seq_group = self.video_sched.scheduler.fcfs_decouple_sjf_schedule()
         #seq_groups = self.video_sched.scheduler.window_based_sjf_with_sjf_update_schedule()
